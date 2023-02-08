c262e7709a3cebd9d7c573a4e03fea4daa4c2b70
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-02-08 14:45:17 +0530

    Sidebar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,104 @@
-import { Box } from "@mui/material";
+import { AccountBoxRounded, ArticleRounded, GroupsRounded, HomeRounded, NightsStayRounded, PersonRounded, SettingsOutlined, StorefrontOutlined } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
     return (
-        <Box 
-        bgcolor="skyblue" 
-        flex={1} 
-        p={2} 
-        sx={{ display: {xs: "none", sm: "block"}}}
+        <Box
+            flex={1}
+            p={2}
+            sx={{ display: { xs: "none", sm: "block" } }}
         >
-            Sidebar
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#home">
+                        <ListItemIcon>
+
+                            <HomeRounded/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Home Page" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#Pages">
+                        <ListItemIcon>
+
+                            <ArticleRounded/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#groups">
+                        <ListItemIcon>
+
+                            <GroupsRounded/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#marketplace">
+                        <ListItemIcon>
+
+                            <StorefrontOutlined/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Marketplace" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#friends">
+                        <ListItemIcon>
+
+                            <PersonRounded/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Friends" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#settings">
+                        <ListItemIcon>
+
+                            <SettingsOutlined/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#profile">
+                        <ListItemIcon>
+
+                            <AccountBoxRounded/>
+
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton LinkComponent="a" href="#profile">
+                        <ListItemIcon>
+
+                            <NightsStayRounded/>
+
+                        </ListItemIcon>
+                        <Switch/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+
         </Box>
     )
 }
