0f61fc9ecbc07ef5c951d27b3cc76684f98915d8
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-02-08 13:38:41 +0530

    completed nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Button } from "@mui/material";
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import { Box, Stack } from "@mui/material";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<SettingsRoundedIcon/>} variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </div>
+    <Box>
+      <Navbar/>
+
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import { Diversity2Rounded, Mail, Notifications } from "@mui/icons-material";
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, Stack, styled, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between"
+});
+
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%"
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+    display: "none",
+    alignItems: "center",
+    gap: "20px",
+    [theme.breakpoints.up("sm")]: {
+        display: "flex"
+    }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+        display: "none"
+    }
+}));
+
+const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Box>
+                    <Stack direction="row" spacing={1} justifyContent="space-between">
+                        <Diversity2Rounded sx={{ display: { xs: "block", sm: "block" } }} />
+                        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>Friendsbook</Typography>
+                    </Stack>
+                </Box>
+                <Search><InputBase placeholder="search..." /></Search>
+
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+
+                    <Badge badgeContent={6} color="error">
+                        <Notifications />
+                    </Badge>
+
+                    <Avatar
+                        sx={{ width: 30, height: 30 }}
+                        src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+                        onClick={e=>setOpen(true)}
+                    />
+                </Icons>
+
+                <UserBox onClick={e=>setOpen(true)}>
+                    <Avatar
+                        sx={{ width: 30, height: 30 }}
+                        src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+                    />
+                    <Typography variant="span">John</Typography>
+
+                </UserBox>
+
+            </StyledToolbar>
+
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={e=>setOpen(false)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuItem >Profile</MenuItem>
+                <MenuItem >My account</MenuItem>
+                <MenuItem >Logout</MenuItem>
+            </Menu>
+        </AppBar>
+    )
+}
+
+export default Navbar;
